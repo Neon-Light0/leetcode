7070d8d37256e987e5ac55729062b494b2055578
Brian Ruan <brian.ruan@convergentimaging.com>
2022-07-07 19:12:49 +0000

    Code challenge submission for question: 704 Binary Search

---
+++ 704 Binary Search/704binary-search.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
+        l = len(nums)
+        
+        left = 0
+        right = l - 1
+
+        while left <= right:
+            mid = (left + right) / 2
+            
+            if nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                return mid
+        
+        return -1
+            
+            
+        
+        
