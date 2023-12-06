bf41dc0d32b695efc15cfaca81f6229c9c049af6
Brian Ruan <brian.ruan@convergentimaging.com>
2023-12-06 00:50:19 +0000

    Code challenge submission for question: 1716 Calculate Money in Leetcode Bank

---
+++ 1716 Calculate Money in Leetcode Bank/1716calculate-money-in-leetcode-bank.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int totalMoney(int n) {
+        int sum = 0;
+
+        for (std::size_t i = 0; i < n; i++){
+            sum += 1 + i / 7 + i % 7;
+        }
+
+        return sum;
+    }
+};
