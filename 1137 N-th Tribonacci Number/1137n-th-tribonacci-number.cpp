2aadc6dc5ab6b1abffb1bb44380c837879b4e693
Brian Ruan <brian.ruan@convergentimaging.com>
2023-12-29 20:52:50 +0000

    Code challenge submission for question: 1137 N-th Tribonacci Number

---
+++ 1137 N-th Tribonacci Number/1137n-th-tribonacci-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if (n == 0 ) return 0;
+        if (n == 1 ) return 1;
+        if (n == 2 ) return 1;
+        
+        int t0 = 0, t1 = 1, t2 = 1;
+        
+        for (std::size_t i = 2; i < n; i++){
+            int temp = t2;
+            t2 = t2 + t1 + t0;
+            t0 = t1;
+            t1 = temp;
+        }
+        
+        return t2;
+    }
+};
