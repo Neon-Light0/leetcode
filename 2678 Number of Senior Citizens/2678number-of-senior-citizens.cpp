0f88e34d888a2bbbce9fe49d2d88aeb420277441
Brian Ruan <brian.ruan@convergentimaging.com>
2024-08-06 23:58:24 +0000

    Code challenge submission for question: 2678 Number of Senior Citizens

---
+++ 2678 Number of Senior Citizens/2678number-of-senior-citizens.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        int count = 0;
+
+        for (std::string& detail : details){
+            int age = std::stoi(detail.substr(11,2));
+
+            if (age > 60){
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
