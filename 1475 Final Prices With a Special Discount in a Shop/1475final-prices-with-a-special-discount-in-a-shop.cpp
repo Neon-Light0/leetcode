d03f3acebe758449957c250004dd4c05b077c735
Brian Ruan <brian.ruan@convergentimaging.com>
2024-12-18 08:18:42 +0000

    Code challenge submission for question: 1475 Final Prices With a Special Discount in a Shop

---
+++ 1475 Final Prices With a Special Discount in a Shop/1475final-prices-with-a-special-discount-in-a-shop.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+        std::vector<int> finalPrices;
+
+        for (int i = 0; i < prices.size(); i++){
+            int discount = 0;
+
+            for (int j = i + 1; j < prices.size(); j++){
+                if (prices[j] <= prices[i]){
+                    discount = prices[j];
+                    break;
+                }
+            }
+
+            finalPrices.push_back(prices[i] - discount);
+        }
+
+        return finalPrices;
+    }
+};
