7e174ed9f227daf826280d0cd3124deba026b4c5
Brian Ruan <brian.ruan@convergentimaging.com>
2025-01-06 16:49:30 +0000

    Code challenge submission for question: 1837 Sum of Digits in Base K

---
+++ 1837 Sum of Digits in Base K/1837sum-of-digits-in-base-k.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int sumBase(int n, int k) {
+        int sum = 0;
+
+        while (n != 0){
+            int digit = n % k;
+            sum += digit;
+            n = (n - digit) / k;
+        }
+
+        return sum;
+    }
+};
