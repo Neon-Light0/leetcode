8a4a5af989c00a6bb3bbdc824936bda6034ab607
Brian Ruan <brian.ruan@convergentimaging.com>
2025-05-06 05:41:29 +0000

    Code challenge submission for question: 1920 Build Array from Permutation

---
+++ 1920 Build Array from Permutation/1920build-array-from-permutation.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        std::vector<int> answer(nums.size());
+
+        for (int i = 0; i < nums.size(); i++){
+            answer[i] = nums[nums[i]];
+        }
+
+        return answer;
+    }
+};
