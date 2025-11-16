e15cf7617af771bd0dd33fb7c3cf46014215506e
Brian Ruan <brian.ruan@convergentimaging.com>
2025-11-16 21:19:26 +0000

    Code challenge submission for question: 1513 Number of Substrings With Only 1s

--- 1513 Number of Substrings With Only 1s/1513number-of-substrings-with-only-1s.cpp
+++ 1513 Number of Substrings With Only 1s/1513number-of-substrings-with-only-1s.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int numSub(string s) {
-        int count = 0;
-        int mod = 1e9 + 7;
-        long long ones = 0;
+        int res = 0;
+        int ones = 0;
+        int modulo = 1e9 + 7;
 
         for (int i = 0; i < s.size(); i++){
             if (s[i] == '1'){
                 ones += 1;
-            } 
-            
-            if  (s[i] != '1' || i == s.size() - 1){
-                count = (count + (ones * (ones + 1) /2)) % mod;  
+            } else {
                 ones = 0;
             }
+
+            res = res % modulo + ones;
         }
 
-        return count;
+        return res;
     }
 };
