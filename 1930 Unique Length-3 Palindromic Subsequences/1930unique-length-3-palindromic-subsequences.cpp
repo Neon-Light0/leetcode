130ad6d0732519591902db1eb01c4066efd00202
Brian Ruan <brian.ruan@convergentimaging.com>
2025-11-21 05:35:29 +0000

    Code challenge submission for question: 1930 Unique Length-3 Palindromic Subsequences

--- 1930 Unique Length-3 Palindromic Subsequences/1930unique-length-3-palindromic-subsequences.cpp
+++ 1930 Unique Length-3 Palindromic Subsequences/1930unique-length-3-palindromic-subsequences.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        std::unordered_map<char, int> leftMap , rightMap;
-        std::unordered_set<std::string> found;
+        std::unordered_set<char> chars;
+        int count = 0;
 
-        leftMap[s[0]] += 1;
-
-        for (int i = 2; i < s.size(); i++){
-            rightMap[s[i]] += 1;
+        for (char& c : s){
+            chars.insert(c);
         }
 
-        for (int i = 1; i < s.size() - 1; i++){
-            for (auto& [c, count] : leftMap){
-                std::string seq;
-                seq += c;
-                seq += s[i];
-                seq += c;
+        for (auto& c : chars){
+            int left = -1;
+            int right = 0;
 
-                if (rightMap.contains(c)){
-                    found.insert(seq);
+            for (int i = 0; i < s.size(); i++){
+                if (c == s[i]){
+                    if (left == - 1){
+                        left = i;
+                    }
+
+                    right = i;
                 }
             }
-        
-            leftMap[s[i]] += 1;
 
-            if (rightMap.contains(s[i+1])){
-                if (rightMap[s[i+1]] <= 1){
-                    rightMap.erase(s[i+1]);
-                } else {
-                    rightMap[s[i+1]] -= 1;
-                }
+            std::unordered_set<char> betweenChars;
+
+            for (int i = left + 1; i < right; i++){
+                betweenChars.insert(s[i]);
             }
+
+            count += betweenChars.size();
         }
 
-        return found.size();
+        return count;
     }
 };
