a29438738c2a88e7a6f55bd3345747abce8bf0b8
Brian Ruan <brian.ruan@convergentimaging.com>
2025-12-20 01:12:41 +0000

    Code challenge submission for question: 944 Delete Columns to Make Sorted

---
+++ 944 Delete Columns to Make Sorted/944delete-columns-to-make-sorted.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int count = 0;
+
+        for (int i = 0; i < strs[0].size(); ++i){
+            for (int j = 0; j + 1 < strs.size(); ++j){
+                if (strs[j][i] > strs[j+1][i]){
+                    count += 1;
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }
+};
