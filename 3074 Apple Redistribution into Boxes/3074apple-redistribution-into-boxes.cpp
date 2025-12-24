fdc3865bec088f74edb5ef409f8b45ee950d4fe8
Brian Ruan <brian.ruan@convergentimaging.com>
2025-12-24 05:01:28 +0000

    Code challenge submission for question: 3074 Apple Redistribution into Boxes

--- 3074 Apple Redistribution into Boxes/3074apple-redistribution-into-boxes.cpp
+++ 3074 Apple Redistribution into Boxes/3074apple-redistribution-into-boxes.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
+        int count = 0;
         std::sort(capacity.begin(), capacity.end(), std::greater<int>());
-        int nBoxes = 0;
-        int nApples = 0;
+        int total = std::accumulate(apple.begin(), apple.end(), 0);
 
-        for (int i = 0; i < apple.size(); i++){
-            nApples += apple[i];
-        }
-
-        for (int i = 0; i < capacity.size(); i++){
-            if (nApples > 0){
-                nBoxes += 1;
+        for (auto& c : capacity){
+            if (total > 0) {
+                total -= c;
+                count += 1;
             } else {
-                break;
+                return count;
             }
-
-            nApples -= capacity[i];
         }
 
-        return nBoxes;
+        return count;
     }
 };
