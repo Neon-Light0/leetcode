30559ef0ac50e6f0fad934d204f3b38c69bcec7a
Brian Ruan <brian.ruan@convergentimaging.com>
2026-01-02 00:23:30 +0000

    Code challenge submission for question: 961 N-Repeated Element in Size 2N Array

--- 961 N-Repeated Element in Size 2N Array/961n-repeated-element-in-size-2n-array.cpp
+++ 961 N-Repeated Element in Size 2N Array/961n-repeated-element-in-size-2n-array.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int repeatedNTimes(vector<int>& nums) {
-        std::unordered_map<int, int> count;
-        int n = nums.size() / 2;
+        int num1 = nums[0], num2 = nums[1], num3 = nums[2];
 
-        for (auto& num : nums){
-            count[num] += 1;
-        }
-
-        for (auto [key, val] : count){
-            if (val == n){
-                return key;
+        for (int i = 3; i < nums.size(); i++){
+            if (nums[i] == num1 || nums[i] == num2 || nums[i] == num3){
+                return nums[i];
             }
+
+            num1 = nums[i-2];
+            num2 = nums[i- 1];
+            num3 = nums[i];
         }
 
-         return -1;
+        return -1;
     }
 };
